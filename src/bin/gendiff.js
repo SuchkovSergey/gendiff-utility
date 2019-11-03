bc7e73f23dc3850521990058c1714efba1a0396d
Sergei Suchkov <rafaelfederer335@gmail.com>
2019-11-03 17:33:51 +0300

    version 0.0.2

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,5 +4,14 @@ const program = require('commander');
 
 program
   .version('0.0.1')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    firstConfigValue = firstConfig;
+    secondConfigValue = secondConfig;
+  });
+
+program
   .description('Compares two configuration files and shows a difference.')
-  .parse(process.argv);
+  .option('-f, --format [type]', 'Output format');
+
+program.parse(process.argv);
