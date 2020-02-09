9f21204107b97d104a5b3117684b4dabf7250c35
Sergei Suchkov <rafaelfederer335@gmail.com>
2020-02-09 20:24:47 +0300

    version 0.2.2.16

--- src/formatters/branch.js
+++ src/formatters/branch.js
@@ -3,10 +3,10 @@ import objStringify from '../utils';
 const render = (ast, indent = '', indent1 = '') => {
   const newIndent = `${indent}  `;
   const reducer = (acc, node) => {
-    const before = node.valueBefore instanceof Object
-      ? objStringify(node.valueBefore, newIndent) : node.valueBefore;
-    const after = node.valueAfter instanceof Object
-      ? objStringify(node.valueAfter, newIndent) : node.valueAfter;
+    const strBuild = (valueType) => (node[valueType] instanceof Object
+      ? objStringify(node[valueType], newIndent) : node[valueType]);
+    const before = strBuild('valueBefore');
+    const after = strBuild('valueAfter');
     const state = node.currentState;
     const strPlus = `${newIndent}+ ${node.name}: ${after}\r\n`;
     const strMinus = `${newIndent}- ${node.name}: ${before}\r\n`;
@@ -15,22 +15,12 @@ const render = (ast, indent = '', indent1 = '') => {
     const strOneNew = {
       deleted: strMinus,
       changedInside: `${newIndent}  ${node.name}: ${render(node.children, ind2, ind2)}\r\n`,
-      changedObj: strMinus,
-      changed: strMinus,
+      changedObj: `${strMinus}${strPlus}`,
+      changed: `${strMinus}${strPlus}`,
       unchanged: strUnchanged,
-      added: '',
-    };
-    const strTwoNew = {
-      deleted: '',
-      changedInside: '',
-      changedObj: strPlus,
-      changed: strPlus,
-      unchanged: '',
       added: strPlus,
     };
-    const strOne = strOneNew[state];
-    const strTwo = strTwoNew[state];
-    return `${acc}${strOne}${strTwo}`;
+    return `${acc}${strOneNew[state]}`;
   };
   return `${ast.reduce(reducer, '{\r\n')}${indent1}}`;
 };
